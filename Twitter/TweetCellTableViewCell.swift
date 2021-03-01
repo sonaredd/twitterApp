ef900970f701d4d88c87fb12ec670e6bb1182b8c
Sharath Reddy <sharathreddy@Sharaths-MacBook-Air.local>
2021-02-28 22:19:49 -0500

    Initial commit

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Sharath Reddy on 2/28/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var userNameLabel: UILabel!
+    
+    
+    @IBOutlet var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
